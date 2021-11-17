875049462d3765fb4d7f4947ba3b5525b4e4fb79
Dana <dalkwifi@uwo.ca>
2021-11-17 15:50:16 -0800

    add

--- index.js
+++ index.js
@@ -17,11 +17,50 @@ app.post('/login', (req,res) => {
     let message = 'Access Denied';
     
     if(userName == 'admin' && password == '123'){
-        message = 'Welcome';
+        res.redirect('/doodlePage.html'); 
     }
     res.send(message);
 })
 
 
+/*
+app.get('/add-time', (req,res) => {
+    let conn = newConnection();
+    conn.connect();
+    conn.query(`insert into Product values ('${req.query.name}',${req.query.time})`
+            ,(err,rows,fields) => {
+                res.redirect('/timeAvaliable');        
+            } );
 
+    conn.end();
+})
+
+app.get('/timeAvaliable', (request, response) => {
+    let conn=newConnection();
+    conn.connect();
+    let productList;
+    conn.query(`select * from Product`, (err,rows,fields) => {
+
+        if (err)
+            response.send('ERROR: ' +err)
+        else
+        {
+            productList = rows;
+
+            let content ='';
+            for (p of productList)
+            {
+                content += '<div>';
+                content += p.name + ":" + p.time 
+                content += '</div>'
+                content += '\n';
+            }
+
+            response.send(content);
+        }
+    })    
+
+    conn.end();
+})
+*/
 app.listen(80);
---
+++ initDB.js
@@ -0,0 +1,54 @@
+const mysql = require('mysql');
+
+let conn = mysql.createConnection({
+        host:'34.133.120.25',
+        user: 'root',
+        password: 'Icecream12#',
+        database: 'usersDB',
+});
+
+conn.connect();
+
+conn.query(`Drop Table Product`,
+                (err,rows,fields) => {
+                    if (err)
+                        console.log(err);
+                    else
+                        console.log('Table Dropped');
+                }
+            )
+conn.query(`CREATE TABLE Product
+            (
+                Description varchar(100),
+                Price   Dec(6,3),
+                imgPath varchar(100)
+            )
+            ` 
+            , (err,rows,fields) => {
+                if (err)
+                    console.log(err);
+                else
+                    console.log('Table Created');
+            })
+// {"desc":"Table","price":"200","imgPath":"/imgs/Table.jpg"}
+conn.query( `insert into Product values ("Table",200,"/imgs/Table.jpg")`
+            , (err,rows,fields) => {
+                if (err)
+                    console.log(err);
+                else
+                    console.log('One row inserted');
+            });
+
+conn.query( `select * from Product `
+            , (err,rows,fields) => {
+                if (err)
+                    console.log(err);
+                else
+                    console.log('One row inserted');
+                for (r of rows)
+                    console.log(r);
+            });
+
+conn.end();
+
+
